3836de02ef6657e2f911a1280ba3cf99c5e0852b
Anastasia Ozerova <one.andann@mail.ru>
2025-04-27 21:57:40 +0300

    Fixed gitignore

--- .history/scripts/update_20250427204153.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-NAMESPACE="image-classifier"
-
-echo "♻️  Rolling update: image-classifier Deployment"
-kubectl rollout restart deployment image-classifier -n ${NAMESPACE}
-kubectl rollout status deployment image-classifier -n ${NAMESPACE}
-echo "✅ Update complete."
